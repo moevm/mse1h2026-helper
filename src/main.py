2e3095fd974293bb8b26572a7f7369ccf67121ef
Michael <mishan.rus2014@gmail.com>
2026-02-28 00:25:24 +0300

    Добавлена возможность анализировать файлы с помощью Pylint
    Реализована обёртка для линтеров, добавлен конкретный класс для pylint, программа ожидает на вход параметр - путь до анализируемого файла

--- src/main.py
+++ src/main.py
@@ -1,5 +1,54 @@
+import os
+import sys
+from abc import ABC, abstractmethod
+import subprocess
+
+
+class Linter(ABC):
+
+	@abstractmethod
+	def run(self, file_path: str):
+		pass
+
+
+class PylintWrapper(Linter):
+
+	def run(self, file_path: str):
+		process = subprocess.run(
+			['pylint', '--score=n', file_path],
+			capture_output=True,
+			text=True,
+			check=False
+		)
+
+		return process.stdout
+
+
+class LinterFactory:
+	_linters = {'.py': PylintWrapper()}
+
+	@classmethod
+	def get_linter(cls, file_path: str) -> Linter:
+		_, ext = os.path.splitext(file_path)
+		linter = cls._linters.get(ext)
+		if not linter:
+			raise ValueError(f'No linter for file {ext}')
+		return linter
+
+
 def main():
-	print('Hello, World!')
+	if len(sys.argv) < 2:
+		print(f'Usage: {sys.argv[0]} <file_path>')
+		sys.exit(1)
+
+	file_to_check = sys.argv[1]
+	try:
+		linter_instance = LinterFactory.get_linter(file_to_check)
+		result = linter_instance.run(file_to_check)
+		print(result)
+	except Exception as e:
+		print(f'Error: {e}')
+		sys.exit(1)
 
 
 if __name__ == '__main__':
