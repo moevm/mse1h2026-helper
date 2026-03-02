f942d26d3fba6b4a8778b71cc393982a41115746
Michael <mishan.rus2014@gmail.com>
2026-03-02 19:03:54 +0300

    Добавлены проверки на критические ошибки Pylint, вывод сообщений об ошибках при необходимости, удалены лишние зависимости в requirements.txt
    Добавлена обработка process.returncode == 1 - фатальная ошибка была отнесена к классу RuntimeError. Изменены возвращаемые значения функции run в интерфейсе Linter. Добавлена обработка process.stderr

--- src/main.py
+++ src/main.py
@@ -2,11 +2,12 @@ import os
 import subprocess
 import sys
 from abc import ABC, abstractmethod
+from typing import Tuple
 
 
 class Linter(ABC):
 	@abstractmethod
-	def run(self, file_path: str):
+	def run(self, file_path: str) -> Tuple[str | None, str | None, int]:
 		pass
 
 
@@ -18,7 +19,12 @@ class PylintWrapper(Linter):
 			text=True,
 			check=False
 		)
-		return process.stdout
+		if process.returncode == 1:
+			if process.stderr is not None and process.stderr != '':
+				raise RuntimeError(f'Pylint analyse finished with Fatal error: {process.stderr}')
+			else:
+				raise RuntimeError(f'Pylint analyse finished with Fatal error')
+		return process.stdout, process.stderr, process.returncode
 
 
 class LinterFactory:
@@ -41,11 +47,17 @@ def main():
 	file_to_check = sys.argv[1]
 	try:
 		linter_instance = LinterFactory.get_linter(file_to_check)
-		result = linter_instance.run(file_to_check)
-		print(result)
+		result, stderr, return_code = linter_instance.run(file_to_check)
+		if stderr is not None and stderr != '':
+			print(f'Linter anasyse finished with error message: {stderr}')
+		if result is not None:
+			print(result)
+		print(f'Returned code: {return_code}')
+	except RuntimeError as e:
+		print(f'Critical error: {e}')
+		sys.exit(1)
 	except Exception as e:
 		print(f'Error: {e}')
-		sys.exit(1)
 
 
 if __name__ == '__main__':
